e9de97aa7711116415dfe0b7808405bbf08884c3
Josh Whetton <josh@whetton.net>
2022-07-29 21:01:56 -0400

    Change note mapping to ease lookups.

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 type MSQ struct {
-	Notes []interface{} `json:"notes"`
-	Loop  bool          `json:"loop"`
-	End   int           `json:"end"`
-	Tempo interface{}   `json:"tempo"`
-	Beats int           `json:"beats"`
+	Notes [][]interface{} `json:"notes"`
+	Loop  bool            `json:"loop"`
+	End   int             `json:"end"`
+	Tempo interface{}     `json:"tempo"`
+	Beats int             `json:"beats"`
 }
 
 func (m *MSQ) Log() {
@@ -24,29 +24,37 @@ func (m *MSQ) Log() {
 }
 
 func main() {
-	var notes = map[string][]int{
-		"NOTE_B3":  {12},
-		"NOTE_C4":  {11},
-		"NOTE_CS4": {74, 139},
-		"NOTE_D4":  {10},
-		"NOTE_DS4": {73, 138},
-		"NOTE_E4":  {9},
-		"NOTE_F4":  {8},
-		"NOTE_FS4": {71, 136},
-		"NOTE_G4":  {7},
-		"NOTE_GS4": {70, 135},
-		"NOTE_A4":  {6},
-		"NOTE_AS4": {69, 134},
-		"NOTE_B4":  {5},
-		"NOTE_C5":  {4},
-		"NOTE_CS5": {67, 132},
-		"NOTE_D5":  {3},
-		"NOTE_DS5": {66, 131},
-		"NOTE_E5":  {2},
-		"NOTE_F5":  {1},
-		"NOTE_FS5": {64, 129},
-		"NOTE_G5":  {0},
-		"NOTE_GS5": {128},
+	var notes = map[int]string{
+		0:   "NOTE_G5",
+		1:   "NOTE_F5",
+		2:   "NOTE_E5",
+		3:   "NOTE_D5",
+		4:   "NOTE_C5",
+		5:   "NOTE_B4",
+		6:   "NOTE_A4",
+		7:   "NOTE_G4",
+		8:   "NOTE_F4",
+		9:   "NOTE_E4",
+		10:  "NOTE_D4",
+		11:  "NOTE_C4",
+		12:  "NOTE_B3",
+		64:  "NOTE_FS5",
+		66:  "NOTE_DS5",
+		67:  "NOTE_CS5",
+		69:  "NOTE_AS4",
+		70:  "NOTE_GS4",
+		71:  "NOTE_FS4",
+		73:  "NOTE_DS4",
+		74:  "NOTE_CS4",
+		128: "NOTE_GS5",
+		129: "NOTE_FS5",
+		131: "NOTE_DS5",
+		132: "NOTE_CS5",
+		134: "NOTE_AS4",
+		135: "NOTE_GS4",
+		136: "NOTE_FS4",
+		138: "NOTE_DS4",
+		139: "NOTE_CS4",
 	}
 
 	filename := flag.String("file", "", "File to parse")
